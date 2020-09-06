ac39bfc5365a09c7f2ba9277624ec8df8c3e9566
Maor <maorba6@gmail.com>
2020-09-06 10:48:41 +0300

    Updates

---
+++ precache-manifest.ffd863ba8c56aedefbc8f4d9086964a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee00a6ba3e37b5936b317838f105b5c",
+    "url": "/XL-SHOP/index.html"
+  },
+  {
+    "revision": "039e0b0bfb42a1e88845",
+    "url": "/XL-SHOP/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/XL-SHOP/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/XL-SHOP/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039e0b0bfb42a1e88845",
+    "url": "/XL-SHOP/static/js/main.729f9713.chunk.js"
+  },
+  {
+    "revision": "74d333508d38c33fad98",
+    "url": "/XL-SHOP/static/js/runtime-main.62f6dc30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/XL-SHOP/static/media/logo.5d5d9eef.svg"
+  }
+]);

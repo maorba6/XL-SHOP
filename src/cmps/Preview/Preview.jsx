596d5a73cb7b2d7c2bd3a2ab8b0b0ed538545d0e
adi_peled <adipeled499@gmail.com>
2020-09-06 12:50:20 +0300

    p

--- src/cmps/Preview/Preview.jsx
+++ src/cmps/Preview/Preview.jsx
@@ -1,18 +1,30 @@
 import React, { useEffect } from 'react';
 import './Preview.scss'
-export function Preview() {
+export function Preview(props) {
+
+    const { item } = props
+
     useEffect(() => {
-        console.log('created');
+        console.log(props);
         return () => {
             console.log('dead');
         }
     }, [])
-
     return (
         <div className="preview">
 
-            preview
-
+            <h1>{item.type}</h1>
+            <div className="price">
+                <span>price:</span>
+                <span>${item.price}</span>
+            </div>
+            <div className="size">
+                <span>size:</span>
+                <span>{item.size}</span>
+            </div>
+            <div className="img">
+                <img src={item.imgUrl} />
+            </div>
         </div>
     );
 }
--- src/services/itemService.js
+++ src/services/itemService.js
@@ -5,35 +5,38 @@ export const itemService = {
 
 const items = [
     {
+        _id: "101",
         type: "shirt",
         color: "red" | "black",
         size: "XXL",
         imgUrl: "*****",
         category: "sport",
         brand: "adidas",
-        price: 15,
+        price: 22,
         discount: 12,
         onStock: true / false
     },
     {
+        _id: "102",
         type: "pants",
         color: "red" | "black",
         size: "XXL",
         imgUrl: "*****",
         category: "casual",
         brand: "nike",
-        price: 15,
+        price: 17,
         discount: 12,
         onStock: true / false
     },
     {
+        _id: "103",
         type: "shoes",
         color: "red",
         size: "44",
         imgUrl: "*****",
         category: "sport",
         brand: "nike",
-        price: 15,
+        price: 76,
         discount: 12,
         onStock: true / false
     }

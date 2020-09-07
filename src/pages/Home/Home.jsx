c75cd28431496635e4aa65b5dbf2d19e694bc260
Maor <maorba6@gmail.com>
2020-09-07 10:03:33 +0300

    שדגדשג

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,8 +11,13 @@ export function Home() {
 
     return (
         <section className="home">
-            <h1>welcome user</h1>
-            <img className="front-img" src={img1}/>
+            {/* <h1>welcome user</h1> */}
+            {/* <img className="front-img" src={img1}/> */}
+            <h2>Catagories</h2>
+            <section>
+                Categories list (['Shirts','Pants','Big','Small']+imgs) 
+                <p>Best Sellers</p>
+            </section>
         </section>
     );
 }

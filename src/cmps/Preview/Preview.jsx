205a4a45e3fd7d363dc9d448136b6431176df474
Maor <maorba6@gmail.com>
2020-09-10 13:33:18 +0300

    ZZZZ

--- src/cmps/Preview/Preview.jsx
+++ src/cmps/Preview/Preview.jsx
@@ -13,15 +13,13 @@ export function Preview({ item, removeItem }) {
     }, [])
     return (
         <div className="preview flex">
-            <Link to={`item/${item._id}`} >
+            <p className="delete-item" onClick={() => removeItem(item._id)} src={trash}>X</p>
+            <div className="flex img-container">
+                <Link to={`item/${item._id}`} >
                     <img className="img-item" src={item.imgUrl} />    
-                <div className="price">
-                    <span>price:</span>
-                    <span>${item.price}</span>
-                </div>
-        
-            </Link>
-            <img className="img-trash" onClick={() => removeItem(item._id)} src={trash} />
+                </Link>
+            </div>
+                <p className="item-price">price: ${item.price}</p>
         </div>
     );
 }

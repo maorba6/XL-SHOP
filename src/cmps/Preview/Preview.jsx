0850dc8b4b88e203e8f52b10c7cc23a0448dad09
adi_peled <adipeled499@gmail.com>
2020-09-21 18:07:42 +0300

    added if to edit and x

--- src/cmps/Preview/Preview.jsx
+++ src/cmps/Preview/Preview.jsx
@@ -1,24 +1,35 @@
 import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
 import trash from '../../assets/trash.png';
+import { connect } from 'react-redux';
+
+
 import './Preview.scss'
 
-export function Preview({ item, removeItem }) {
-
-
-    useEffect(() => {
-        return () => {
-        }
-    }, [])
+function _Preview(props) {
+    const { item, removeItem } = props
+    const user = JSON.parse(props.user)
+  
+    
     return (
         <div className="preview flex">
-            <p className="delete-item" onClick={() => removeItem(item._id)} src={trash}>X</p>
+            {user.isAdmin && <p className="delete-item" onClick={() => removeItem(item._id)} src={trash}>X</p>}
             <div className="flex img-container">
                 <Link to={`item/${item._id}`} >
-                    <img className="img-item" src={item.imgUrl} />    
+                    <img className="img-item" src={item.imgUrl} />
                 </Link>
             </div>
-                <p className="item-price">price: ${item.price}</p>
+            <p className="item-price">price: ${item.price}</p>
         </div>
     );
 }
+
+
+function mapStateProps(state) {
+    return {
+        user: state.userReducer.user,
+    }
+}
+const mapDispatchToProps = {
+}
+export const Preview = connect(mapStateProps, mapDispatchToProps)(_Preview)

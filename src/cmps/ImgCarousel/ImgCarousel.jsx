8395f46f31e1e8f11f78641d4227334147f27514
adi_peled <adipeled499@gmail.com>
2020-09-30 14:32:03 +0300

    carusel

--- src/cmps/ImgCarousel/ImgCarousel.jsx
+++ src/cmps/ImgCarousel/ImgCarousel.jsx
@@ -4,47 +4,47 @@ import './ImgCarousel.scss'
 import 'react-multi-carousel/lib/styles.css';
 
 export default function ImgCarousel(props) {
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-          slidesToSlide: 1// optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-          slidesToSlide: 2 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        }
-      };
-    return (
-        <div>
-           <Carousel
-                swipeable={false}
-                draggable={false}
-                showDots={true}
-                responsive={responsive}
-                ssr={false} // means to render carousel on server-side.
-                infinite={false}
-                // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-                autoPlaySpeed={1000}
-                keyBoardControl={true}
-                customTransition="all .5"
-                transitionDuration={500}
-                containerClass="carousel-container"
-                removeArrowOnDeviceType={["tablet", "mobile"]}
-                // deviceType={this.props.deviceType}
-                dotListClass="custom-dot-list-style"
-                itemClass=""
-                >
-                <img src={props.imgs[0]} alt=""/>
-                <img src={props.imgs[1]} alt=""/>
-                <img src={props.imgs[2]} alt=""/>
-            </Carousel><span>;</span>
-        </div>
-    )
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+      slidesToSlide: 1// optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
+  return (
+    <div>
+      <Carousel
+        swipeable={false}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        ssr={false} // means to render carousel on server-side.
+        infinite={false}
+        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        // deviceType={this.props.deviceType}
+        dotListClass="custom-dot-list-style"
+        itemClass=""
+      >
+      
+        {props.imgs.map(imgUrl => {
+          return <img src={imgUrl} />
+        })}
+      </Carousel><span>;</span>
+    </div>
+  )
 }

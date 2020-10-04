30c62d5fa5b6b7a21881a1e15455a7ca639e8918
Maor <maorba6@gmail.com>
2020-10-04 18:00:47 +0300

    דגכ

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function Cart() {
+ function _Cart(props) {
+    const user = props.user
+    console.log(user);
     return (
         <div>
-            
+           {props.user && <div>{user.email}</div>} 
         </div>
     )
 }
+
+function mapStateProps(state) {
+    return {
+        user: state.userReducer.user,
+    }
+}
+const mapDispatchToProps = {
+}
+
+export const Cart = connect(mapStateProps, mapDispatchToProps)(_Cart)

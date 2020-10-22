8ad9c677adb31564329536d8bae5d4ccf871adee
adi <adipeled@gmail.com>
2020-10-22 12:59:14 +0300

    h

--- src/cmps/List/List.jsx
+++ src/cmps/List/List.jsx
@@ -4,16 +4,16 @@ import './List.scss'
 export function List({ items, removeItem, toggleLike }) {
 
 
-
+    console.log({ items });
     return (
         < section >
-            < div className="item-list">
+            {items && < div className="item-list">
                 {
                     items.map(item =>
                         < Preview key={item._id} item={item} toggleLike={toggleLike} removeItem={removeItem} />
                     )
                 }
-            </div >
+            </div >}
         </section >
     );
 }

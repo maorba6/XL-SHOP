36b13cf2c80ff1494576e262b6931fd29c290d2e
maorba6 <you@example.commaorba6@gmail.com>
2021-01-04 10:38:50 +0200

    base64 upload img

--- src/cmps/UploadImg/UploadImg.jsx
+++ src/cmps/UploadImg/UploadImg.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FileBase from 'react-file-base64'
 import { uploadImg } from '../../services/uploadImg.js'
 import './UploadImg.scss'
 export default function UploadImg(props) {
 
-    async function showImgUrl(event) {
-        const file = event.target.files[0]
-        await uploadImg(file)
-            .then(res => props.uploadImg(res.url))
+    // async function showImgUrl(event) {
+    //     const file = event.target.files[0]
+    //     await uploadImg(file)
+    //         .then(res => props.uploadImg(res.url))
+    // }
+
+
+    function testImg(base64){
+        console.log('hi');
+        console.log(base64.base64);
+        props.uploadImg(base64.base64)
     }
+
     return (
         <div>
-            <input className="upload-inp" type="file" onChange={(event) => showImgUrl(event)} />
+            {/* <input className="upload-inp" type="file" onChange={(event) => showImgUrl(event)} /> */}
+            <FileBase type ="file"
+            multiple={false}
+            onDone = {(base64) => testImg(base64)}
+            />
         </div>
     )
 }
